c5c34131e610d063c92035e1748d9f980c31a520
Fusu <arkadiusz.fus.95@gmail.com>
2019-07-24 20:18:44 +0200

    added rollers sprites to the game

--- src/classes/Roller.ts
+++ src/classes/Roller.ts
@@ -1,10 +1,12 @@
 interface iRoller {
     symbols: Array<String>;
+    group: Phaser.Group
 }
 
 module App {
 	export class Roller implements iRoller {
 		symbols: Array<String>
+		group: Phaser.Group
 		constructor(symbols: Array<String>) {
 			this.symbols = symbols;
 		}
--- src/states/Gameplay.ts
+++ src/states/Gameplay.ts
@@ -3,16 +3,36 @@ module App {
 		background: Phaser.Sprite;
 		rollers: Array<Roller>;
 		create() {
-			var that = this;
-			this.rollers = [];
-
 			this.background = this.game.add.sprite(0, 0, 'main', 'bg'); 
 			this.background.width = this.game.width;
 			this.background.height = this.game.height;
 		
+			this._initRollers();
+		}
+		_initUI = function () {
+
+		}
+		_initRollers = function () {
+			var that = this;
+			this.rollers = [];
+
 			this.game.cache.getJSON('config').forEach(function (item: Array<String>) {
 				that.rollers.push(new Roller(item));
 			});
+			
+			this.rollers.forEach(function(roller: Roller, index: number) {
+				roller.group = that.add.group();
+
+				roller.symbols.forEach(function(symbol: String) {
+					var _sprite = that.add.sprite(0, 0, 'symbols', symbol + "_000"); 
+					roller.group.add(_sprite);
+				});
+
+				roller.group.align(1, -1, 250, 200);
+				roller.group.x = that.game.width * 0.5 + roller.group.width / 2 - (300 * index); 
+			});
+
+			console.log(this.rollers)
 		}
 	}
 } 
